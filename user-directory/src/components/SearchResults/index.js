1172897c6dcfb4878287d92f69d39c6f3eb7c4ad
Hema <khemalatha507@gmail.com>
2020-09-21 12:27:28 -0400

    working on components directory pages

---
+++ user-directory/src/components/SearchResults/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function SearchResults(props) {
+
+  return (
+    <ul className="list-group search-results">
+        <li className="list-group-item" key={props.id}>
+          {props.img}
+          {props.name}
+          {props.email}
+          {props.phone}
+          {props.city}
+          {props.state}
+        </li>
+    </ul>
+  )}
+
+
+export default SearchResults;
