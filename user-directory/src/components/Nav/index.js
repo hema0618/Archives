58ae405cefa744a187fc25d66822fab7a62b00a6
Hema <khemalatha507@gmail.com>
2020-09-21 12:57:24 -0400

    changes in search/wrapper

--- user-directory/src/components/Nav/index.js
+++ user-directory/src/components/Nav/index.js
@@ -1,35 +1,81 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
+import SearchBox from "../SearchBox";
+import "./Nav.css";
+
+function Nav({ handleSearchChange }) {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse row" id="navbarNav">
+          <div className="input-group col-8">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="">
+                DOB
+              </span>
+            </div>
+            <input type="date" className="form-control" />
+            <input type="date" className="form-control" />
+          </div>
+          <div className="search-area col-4">
+            <SearchBox handleSearchChange={handleSearchChange} />
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  export default Nav;
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Employee Search
-      </Link>
-      <div>
-        {/* <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/home"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-           
-          </li>
-        </ul> */}
-      </div>
-    </nav>
-  );
-}
+// function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <Link className="navbar-brand" to="/">
+//         Pupster
+//       </Link>
+//       <div>
+//         <ul className="navbar-nav">
+//           <li className="nav-item">
+//             <Link
+//               to="/"
+//               className={
+//                 window.location.pathname === "/" || window.location.pathname === "/about"
+//                   ? "nav-link active"
+//                   : "nav-link"
+//               }
+//             >
+//               About
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link
+//               to="/discover"
+//               className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+//             >
+//               Discover
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link
+//               to="/search"
+//               className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+//             >
+//               Search
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
 
-export default Navbar;
+// export default Navbar;
