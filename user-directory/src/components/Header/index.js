b26c52ab9e502df8582a2b9177bfcc00c9f02f69
Hema <khemalatha507@gmail.com>
2020-09-21 13:31:45 -0400

    fixed bugs in index/changes in Nav

--- user-directory/src/components/Header/index.js
+++ user-directory/src/components/Header/index.js
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header () {
     return (
         <div className="header">
-            <h1>Employee Directory</h1>
-            <p>Click on each heading to filter or use the search box to narrow your results</p>
+            <h1>User Directory</h1>
+            <p>Click on Name to filter the results</p>
         </div>
     );
 }
--- user-directory/src/components/Nav/index.js
+++ user-directory/src/components/Nav/index.js
@@ -3,37 +3,21 @@ import SearchBox from "../SearchBox";
 import "./Nav.css";
 
 function Nav({ handleSearchChange }) {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse row" id="navbarNav">
-          <div className="input-group col-8">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="">
-                DOB
-              </span>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse row" id="navbarNav">
+        <div className="input-group col-8">
+          <div className="input-group-prepend">
+            <div className="search-area col-8">
+              <SearchBox handleSearchChange={handleSearchChange} />
             </div>
-            <input type="date" className="form-control" />
-            <input type="date" className="form-control" />
-          </div>
-          <div className="search-area col-4">
-            <SearchBox handleSearchChange={handleSearchChange} />
           </div>
         </div>
-      </nav>
-    );
-  }
-  export default Nav;
+      </div>
+    </nav>
+  );
+}
+export default Nav;
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 // function Navbar() {
